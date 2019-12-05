8c698f90df18541699f8c98e149a3574072b2826
Ihar Sakun <watsonik@gmail.com>
2019-12-05 16:14:34 +0300

    Module_6: Add 'mangoose' connection

---
+++ news_backend_db/server/db/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'frontcamp';
+
+const connect = () => {
+    mongoose.connect(`${url}/${dbName}`, {useNewUrlParser: true});
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error'));
+    db.once('open', () => {
+        console.log('MongoDB connected');
+    });
+};
+module.exports.connect = connect;
+
+
