4658ff4d5381cf4b8379dbb32493801a736f5e11
Ihar Sakun <watsonik@gmail.com>
2020-01-21 22:57:18 +0300

    9. Angular p1
    Generate logo components, add pics

--- angular1/news/src/app/app.module.ts
+++ angular1/news/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
